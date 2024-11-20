85cc114714769276d6912bf3b1de05341803704e
Jokerking <ludwig.cardona29@gmail.com>
2024-11-20 01:00:17 -0500

    (back-end) se realiza el cliente, el proveedor y el producto)

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProveedoresModule } from './module/proveedores/proveedores.module';
+import { ClientesModule } from './module/clientes/clientes.module';
+import { ProductosModule } from './module/productos/productos.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://juliriveraquintero:3bAwpBHC9iNii@UQ@pruebas.1hkhp.mongodb.net/PruebasNestJS?retryWrites=true&w=majority&appName=Pruebas'),
+    ProveedoresModule,
+    ClientesModule,
+    ProductosModule,
+  ],
+})
+export class AppModule {}
